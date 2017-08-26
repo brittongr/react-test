552edecd7c7f23f1cbcdd8a351ac6033507a1a62
Greivin Britton <brittongr@gmail.com>
2017-08-26 10:36:12 -0600

    Initial source code react app with hot reload

---
+++ dev/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AppContainer } from 'react-hot-loader';
+// AppContainer is a necessary wrapper component for HMR
+
+import { Demo } from '../src/Demo';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('react-root')
+  );
+};
+
+render(Demo);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('../src/Demo', () => {
+    render(Demo)
+  });
+}
---
+++ src/Demo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Demo extends Component
+{
+    render()
+    {
+        return (
+            <div>Hola</div>
+        );
+    }
+}
